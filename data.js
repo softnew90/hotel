7ec8bf1006092f3821cd2fcacd79904f6af9c2f8
softnew90 <softnew90@gmail.com>
2026-08-21 17:18:00 +0300

    Update data.js

--- data.js
+++ data.js
@@ -63,7 +63,7 @@ const siteSettings = {
     heroMedia: {
         type: "image", // جرب تغييرها إلى "image" أو "localVideo"
         src: "assets/2.jpg", // ضع ID اليوتيوب، أو رابط الصورة/الفيديو حسب النوع المختار
-        fallbackImage: "https://lusailresortsa.com/assets/img/hero/home3/bg.jpg" // الصورة التي تظهر قبل التحميل
+        fallbackImage: "" // الصورة التي تظهر قبل التحميل
     },
     
     phoneNumber: "0509187294",
